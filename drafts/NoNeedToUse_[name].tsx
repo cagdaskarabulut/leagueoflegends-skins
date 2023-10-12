e14bb70cb03c05f556e5ba14c42ec43a40b20ef5
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-10-12 16:46:47 +0300

    .

--- drafts/NoNeedToUse_[name].tsx
+++ drafts/NoNeedToUse_[name].tsx
@@ -1,45 +1,45 @@
-import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
-import React from "react";
-import { getHeroByName } from "../data/getHeroByName";
-import { Hero } from "../data/Hero";
+// import { GetServerSideProps } from "next";
+// import { useRouter } from "next/router";
+// import React from "react";
+// import { getHeroByName } from "../data/getHeroByName";
+// import { Hero } from "../data/Hero";
 
-type HeroPageProps = {
-  hero: Hero;
-};
+// type HeroPageProps = {
+//   hero: Hero;
+// };
 
-const HeroPage = (props: HeroPageProps) => {
-  //- show basic(first) image and video 
-  return (
-    <div>
-      HeroPage
-      <h1>{props.hero?.name}</h1>
-      <div>
-        {props.hero?.skins?.map((skinObject) => {
-          return <p key={props.hero?.name+"-"+skinObject.skin}>{skinObject.skin}</p>
-        })}
-      </div>
-    </div>
-  );
-};
+// const HeroPage = (props: HeroPageProps) => {
+//   //- show basic(first) image and video 
+//   return (
+//     <div>
+//       HeroPage
+//       <h1>{props.hero?.name}</h1>
+//       <div>
+//         {props.hero?.skins?.map((skinObject) => {
+//           return <p key={props.hero?.name+"-"+skinObject.skin}>{skinObject.skin}</p>
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default HeroPage;
+// export default HeroPage;
 
-export const getServerSideProps: GetServerSideProps<HeroPageProps> = async (
-  ctx
-) => {
-  const name = ctx.params?.name as string;
+// export const getServerSideProps: GetServerSideProps<HeroPageProps> = async (
+//   ctx
+// ) => {
+//   const name = ctx.params?.name as string;
   
-  const hero = getHeroByName(name) ?? null;
-  if (!hero) {
-    return {
-      notFound: true,
-    };
-  }
+//   const hero = getHeroByName(name) ?? null;
+//   if (!hero) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  return {
-    props: {
-      hero,
-    },
-  };
-};
+//   return {
+//     props: {
+//       hero,
+//     },
+//   };
+// };
--- drafts/TestStorage.js
+++ drafts/TestStorage.js
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from "react";
-//- Örnek olarak yapılmıştır. Pages klasörü altına eklenirse çalışacaktır.
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { storage } from "../utils/firebase";
+// import React, { useEffect, useState } from "react";
+// //- Örnek olarak yapılmıştır. Pages klasörü altına eklenirse çalışacaktır.
+// import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+// import { storage } from "../utils/firebase";
 
-function TestStorage() {
-  const [url, setUrl] = useState(null);
-  const imageRef = ref(storage,"images/image-test");
+// function TestStorage() {
+//   const [url, setUrl] = useState(null);
+//   const imageRef = ref(storage,"images/image-test");
 
-  useEffect(() => {
-    // console.log("imageRef:"+imageRef);
-    getDownloadURL(imageRef)
-      .then((url) => {
-        setUrl(url);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, []);
+//   useEffect(() => {
+//     // console.log("imageRef:"+imageRef);
+//     getDownloadURL(imageRef)
+//       .then((url) => {
+//         setUrl(url);
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//   }, []);
 
-  return (
-    <div>
-      <h1>Firestore / Firebase Storage Test Page</h1>
+//   return (
+//     <div>
+//       <h1>Firestore / Firebase Storage Test Page</h1>
 
-      <input
-        type="file"
-        accept="image/*"
-        onChange={(e) => {
-          const file = e.currentTarget.files[0];
-          uploadBytes(imageRef, file);
-        }}
-      />
+//       <input
+//         type="file"
+//         accept="image/*"
+//         onChange={(e) => {
+//           const file = e.currentTarget.files[0];
+//           uploadBytes(imageRef, file);
+//         }}
+//       />
 
-      {url && <img src={url} alt="some" width={240} />}
-    </div>
-  );
-}
+//       {url && <img src={url} alt="some" width={240} />}
+//     </div>
+//   );
+// }
 
-export default TestStorage;
+// export default TestStorage;
