b19fb7aa3f7c5222db3d208f6060946b791f2032
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-11-04 15:21:18 +0300

    video url added

--- components/mainComponents/Header.js
+++ components/mainComponents/Header.js
@@ -1,15 +1,21 @@
 import styles from "./Header.module.scss";
-import { Chip, Container, Grid, InputAdornment, TextField } from "@mui/material";
+import {
+  Chip,
+  Container,
+  Grid,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
 import React from "react";
 import EmailIcon from "@mui/icons-material/Email";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import SearchPanel from "../reusableComponents/SearchPanel";
 // import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 // import InstagramIcon from "@mui/icons-material/Instagram";
 // import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const Header = ({}) => {
-
   return (
     <Container className={styles.header}>
       <Grid
@@ -20,21 +26,14 @@ const Header = ({}) => {
         alignItems="center"
       >
         <Grid item xs={12}>
-          <h2><a className={styles.logoStyle} href="/">League of Legends - Skins</a></h2>
+          <h2>
+            <a className={styles.logoStyle} href="/">
+              League of Legends - Skins
+            </a>
+          </h2>
         </Grid>
         {/* <Grid item xs={6}>
-          <TextField id="outlined-basic" label="Search in champions and skins" variant="outlined"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-           className={styles.SearchBoxStyle} />
-        </Grid>
-        <Grid item xs={3}>
-
+          <SearchPanel/>
         </Grid> */}
       </Grid>
     </Container>
