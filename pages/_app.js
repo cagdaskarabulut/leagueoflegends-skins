5a49e7233c9bcdbb450ca3b348ece91421696ee8
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-11-07 14:42:47 +0300

    yandex ads added

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,32 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import Script from 'next/script'
-import { useEffect } from 'react'
-import * as gtag from '../lib/gtag'
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 
 const App = ({ Component, pageProps }) => {
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
       <Head>
 
-        <script async 
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8764830534484668"
-        crossorigin="anonymous"></script> 
+
+        {/* Google Ads - Start */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8764830534484668"
+          crossorigin="anonymous"
+        ></script>
 
         <script
           dangerouslySetInnerHTML={{
@@ -37,15 +41,60 @@ const App = ({ Component, pageProps }) => {
             `,
           }}
         />
+        {/* Google - End */}
+
+
+
+
+        {/* Yandex Ads - Start       */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.yaContextCb=window.yaContextCb||[]
+            `,
+          }}
+        />
+        <script src="https://yandex.ru/ads/system/context.js" async></script>
+        {/* Yandex Ads - End       */}
+
+
+
       </Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
+
+
+
+      {/* Google Analytics - Start */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
+      {/* Google Analytics - End */}
+
+
+
+
+      {/* Yandex Ads - Start */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.yaContextCb.push(()=>{
+              Ya.Context.AdvManager.render({
+                "blockId": "R-A-3762197-1",
+                "type": "floorAd",
+                "platform": "desktop"
+              })
+            })
+            `,
+        }}
+      />
+      {/* Yandex Ads - End */}
+
+
+      
+
       <Component {...pageProps} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
