021a57c09e49adfc7112ede7930a72bedac43934
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-11-15 14:15:03 +0300

    new theme

--- pages/index.js
+++ pages/index.js
@@ -25,9 +25,6 @@ export default function HomePage({ allSkinsList }) {
             width: 100%;
             height: 100vh;
             background-size: cover;
-            -ms-animation: animate 30s ease infinite alternate;
-            -webkit-animation: animate 30s ease infinite alternate;
-            -moz-animation: animate 30s ease infinite alternate;
           }
           body::after {
             content: "";
@@ -39,9 +36,12 @@ export default function HomePage({ allSkinsList }) {
             height: 100vh;
             background-size: cover;
             background-color: black !important;
-            opacity: 0.5 !important;
+            opacity: 0.8 !important;
             position: fixed !important;
             z-index: -1;
+            -ms-animation: animate 30s ease infinite alternate;
+            -webkit-animation: animate 30s ease infinite alternate;
+            -moz-animation: animate 30s ease infinite alternate;
           }
           @-webkit-keyframes animate {
             0% {
