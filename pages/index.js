b13b891c61b5b7d650d9fef73ba73183336f188e
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-11-28 09:32:56 +0300

    skin prices added

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function HomePage({ allSkinsList }) {
       <MetaPanel
         title="League of Legends Champion Skins Showcase: Stunning Skin Images and Skin Videos"
         descriptionContent="Explore the little-known stories of all heroes and breathtaking world of League of Legends champion skins through our extensive collection of images and videos. Immerse yourself in the visual splendor of LoL champions as we showcase their incredible skins in action. From legendary to rare, discover the beauty and power of each skin in our curated galleries and videos."
-        keywordsContent="lol skin, lol skins, League of Legends, champion skins, skin images, skin videos, lol skin videos, gaming aesthetics, champion customization, video game visuals, league of legends skin, league of legends skins, , little-known stories of all heroes"
+        keywordsContent="lol skin, lol skins, League of Legends, champion skins, skin images, skin videos, lol skin videos, gaming aesthetics, champion customization, video game visuals, league of legends skin, league of legends skins, little-known stories of all heroes, how many rp, skin prices, skin rp"
         imagePath="/images/lol-skins-icon.ico"
         imageAlt="League of Legends Skins"
       />
