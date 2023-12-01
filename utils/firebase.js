0c7c6aa78453643ef7863bc48d5a62b5a3434718
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-12-01 19:02:32 +0300

    .

--- utils/firebase.js
+++ utils/firebase.js
@@ -8,7 +8,7 @@ import { collection, getFirestore, onSnapshot } from "firebase/firestore";
 const firebaseConfig = {
   projectId: "leagueoflegends-skins-firebase",
   storageBucket: "leagueoflegends-skins-firebase.appspot.com",
-  apiKey: "AIzaSyCEpmmY8u8q6Ow2JaghKJBx2z9MbULBLY0",
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
   measurementId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
